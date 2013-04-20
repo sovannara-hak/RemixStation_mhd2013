f84aee3930e7b45a59fe50d6f61b5f685426ce35
Sovannara Hak <hak.sovannara@gmail.com>
2013-04-20 20:04:16 +0200

    Add analyze beats

--- src/parse_music.py
+++ src/parse_music.py
@@ -4,7 +4,7 @@ import echonest.remix.audio as audio
 class MusicParser(object):
   def __init__(self, file_name):
     self.filename = file_name
-    self.bars = None
+    self.samples = None
     try:
       self.audio_file = audio.LocalAudioFile(self.filename)
     except EOFError:
@@ -12,16 +12,22 @@ class MusicParser(object):
       print "File does not exist"
       return
 
-  def analize_bars(self):
-    self.bars = self.audio_file.analysis.bars
+  def analyze_bars(self):
+    self.samples = []
+    self.samples = self.audio_file.analysis.bars
     self.number_items = len(self.bars)
 
-  def write_bars_sample(self, path, prefix):
+  def analyze_beats(self):
+    self.samples = []
+    self.samples = self.audio_file.analysis.beats
+    self.number_items = len(self.samples)
+
+  def write_sample(self, path, prefix):
     suffix = 0
-    if self.bars is not None:
-      for bar in self.bars:
-	audio.getpieces(self.audio_file, [bar]).encode(path+prefix+str(suffix)+".wav")
+    if self.samples is not None:
+      for sample in self.samples:
+	audio.getpieces(self.audio_file, [sample]).encode(path+prefix+str(suffix)+".wav")
 	suffix += 1
     else:
-      print "Analyze bars first"
+      print "Analyze first"
 
