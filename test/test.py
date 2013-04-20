23ff8a14b52b04415ffb965870e47eac26eddb6b
Sovannara Hak <hak.sovannara@gmail.com>
2013-04-20 19:01:23 +0200

    Update Test

--- test/test.py
+++ test/test.py
@@ -2,6 +2,8 @@ from IPython import embed
 import mhd
 
 
+mhd.pygame.init()
+mhd.mix.pre_init(44100,-16,2,2048)
 mhd.pm.init()
 
 cm = mhd.MidiDevice()
@@ -9,15 +11,27 @@ cm.list_devices()
   
 cm.setDevice(3)
 
+testmp3 = "/home/sovan/compil/julesverne/share/echo-nest-remix-examples/music/Raleigh_Moncrief-Guppies.mp3" 
+path = "/tmp/"
+prefix = "Ral"
+
+mp = mhd.MusicParser(testmp3)
+
+print "Analize..."
+mp.analize_bars()
+print "Write samples..."
+mp.write_bars_sample(path, prefix)
+
+rs = mhd.RemixStation(cm, path, prefix, mp.number_items)
+print "Loading..."
+rs.load()
+#rs.play()
+
 def go():
   while 1:
     msg = cm.device.read(1)
     if len(msg):
-      if msg[0][0][1] == 48 and msg[0][0][0] == 144:
-	print "yo "
-      if msg[0][0][1] == 50 and msg[0][0][0] == 144:
-	print "ya "
-      if msg[0][0][1] == 52 and msg[0][0][0] == 144:
-	break;
+      if msg[0][0][0] == 144:
+	print msg
 embed()
 
