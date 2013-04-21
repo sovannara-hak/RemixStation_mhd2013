80d46c0da498a178a822c730bf7591ccbef24afa
Sovannara Hak <hak.sovannara@gmail.com>
2013-04-21 10:00:37 +0200

    Add shortcut and line command argument

--- test/play.py
+++ test/play.py
@@ -3,6 +3,8 @@ import mhd
 import threading
 import time
 
+import sys
+
 class RS(threading.Thread):
   def __init__(self, rs, name = ''):
     threading.Thread.__init__(self)
@@ -24,14 +26,19 @@ mhd.mix.pre_init(44100,-16,2,2048)
 mhd.pm.init()
 
 cm = mhd.MidiDevice()
-cm.list_devices()
+#cm.list_devices()
   
 cm.setDevice(3)
 
 testmp3 = "/home/sovan/compil/julesverne/share/echo-nest-remix-examples/music/Raleigh_Moncrief-Guppies.mp3" 
 testmp3 = "/home/sovan/paradise.mp3"
-testmp3 = "/home/sovan/blood.m4a"
+#testmp3 = "/home/sovan/coffinb.mp3"
+#testmp3 = "/home/sovan/blood.m4a"
 testmp3 = "/home/sovan/juan.mp3"
+
+if len(sys.argv) == 2:
+  testmp3 = sys.argv[1]
+
 path = "/tmp/"
 prefix = "Ral"
 
@@ -55,6 +62,18 @@ def go():
     if len(msg):
       if msg[0][0][0] == 144:
 	print msg
+
+def rec():
+  rs.rec()
+
+def pause():
+  rs.pause()
+
+def stop():
+  RemixStation.stop()
+
+def render(out):
+  mp.render(rs.log, out)
 
 embed()
 
